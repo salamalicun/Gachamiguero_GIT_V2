56ccb66ea623d053fee3c9f6a0c577a015351be6
salamalicun <608222@alu.murciaeduca.es>
2024-05-20 13:52:53 +0200

    Refactorización clase Main a Gachamiguero

--- Test/Tests_GachaMiga.java
+++ Test/Tests_GachaMiga.java
@@ -4,20 +4,20 @@ import org.junit.Assert;
 public class Tests_GachaMiga {
     @Test
     public void Test_falso() {
-        Assert.assertEquals("1", Main.termomixLidl(1));
+        Assert.assertEquals("1", Gachamiguero.termomixLidl(1));
     }
     @Test
     public void Test_miga() {
-        Assert.assertEquals("Gacha", Main.termomixLidl(3));
+        Assert.assertEquals("Gacha", Gachamiguero.termomixLidl(3));
     }
 
     @Test
     public void Test_gacha() {
-        Assert.assertEquals("Miga", Main.termomixLidl(5));
+        Assert.assertEquals("Miga", Gachamiguero.termomixLidl(5));
     }
 
     @Test
     public void Test_gachamiga() {
-        Assert.assertEquals("GachaMiga", Main.termomixLidl(15));
+        Assert.assertEquals("GachaMiga", Gachamiguero.termomixLidl(15));
     }
 }
--- src/Gachamiguero.java
+++ src/Gachamiguero.java
@@ -1,4 +1,4 @@
-public class Main {
+public class Gachamiguero {
     private static final int CANTIDAD_NUMEROS = 100;
     private static int numero = 0;
     public static void main(String[] args) throws InterruptedException {
