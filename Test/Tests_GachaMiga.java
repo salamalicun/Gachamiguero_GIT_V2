f99016d30631e1c7e6f6a87d580a05a09326c743
salamalicun <608222@alu.murciaeduca.es>
2024-05-20 12:10:09 +0200

    Test01 OK

--- Test/Tests_GachaMiga.java
+++ Test/Tests_GachaMiga.java
@@ -2,4 +2,9 @@ import org.junit.Test;
 import org.junit.Assert;
 
 public class Tests_GachaMiga {
+    @Test
+    public void Test01() {
+        Assert.assertEquals("1", Main.termomixLidl(1));
+    }
+
 }
