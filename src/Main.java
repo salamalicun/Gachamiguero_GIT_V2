4fc37ac3a166835f8db13d72e54df7f1a3b720a6
salamalicun <608222@alu.murciaeduca.es>
2024-05-20 13:42:57 +0200

    Clase Main modificada V3 y Tests OK

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,31 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("GACHAMIGUERO GIT V2\n--------------------");
-        for (int numero = 1; numero <= 100; numero++) {
-            System.out.println(termomixLidl(numero));
+    private static final int CANTIDAD_NUMEROS = 100;
+    private static int numero = 0;
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("GACHAMIGUERO GIT V3\n--------------------");
+        Thread.sleep(100);
+        String load = "";
+        System.out.print("Loading    ( 0% )    ");
+        while (numero <= CANTIDAD_NUMEROS) {
+            load += "#";
+            System.out.print(load);
+            Thread.sleep(200);
+            numero += 10;
         }
+        System.out.print("    ( 100% )    Loaded :)");
+        Thread.sleep(750);
+        numero = 0;
+        System.out.println("\n--------------------");
+
+        while (numero < CANTIDAD_NUMEROS) {
+            ++numero;
+            System.out.println(termomixLidl(numero));
+            Thread.sleep(25);
+        }
+        System.out.println("--------------------");
     }
     public static String termomixLidl(int numero) {
-        String resultado = Integer.toString(numero);
-        if (numero%3 == 0) resultado = "Gacha";
-        if (numero%5 == 0) resultado = "Miga";
-        if (numero%15 == 0) resultado = "GachaMiga";
-        return resultado;
+        return (numero % 3 == 0) ? ((numero % 5 == 0) ? "GachaMiga" : "Gacha")
+                : ((numero % 5 == 0) ? "Miga" : String.valueOf(numero));
     }
 }
