2a1c962adc12d1b7eb1344a05e9737917b9dc80b
salamalicun <608222@alu.murciaeduca.es>
2024-05-20 11:39:19 +0200

    Subida inicial

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("GACHAMIGUERO GIT V2\n--------------------");
+        for (int numero = 1; numero <= 100; numero++) {
+            System.out.println(termomixLidl(numero));
+        }
+    }
+
+    public static String termomixLidl(int numero) {
+
+        return Integer.toString(numero);
+    }
+}
