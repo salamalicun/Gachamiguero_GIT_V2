d93338053c6a189ef167e9be709c8dff4f8d423c
salamalicun <608222@alu.murciaeduca.es>
2024-05-20 13:26:57 +0200

    Tests simplicados a 4 (normal, "Gacha", "Miga" y "GachaMiga")

--- Test/Tests_GachaMiga.java
+++ Test/Tests_GachaMiga.java
@@ -3,67 +3,22 @@ import org.junit.Assert;
 
 public class Tests_GachaMiga {
     @Test
-    public void Test01() {
+    public void Test_falso() {
         Assert.assertEquals("1", Main.termomixLidl(1));
     }
+
     @Test
-    public void Test02() {
-        Assert.assertEquals("2", Main.termomixLidl(2));
-    }
-    @Test
-    public void Test03() {
+    public void Test_miga() {
         Assert.assertEquals("Gacha", Main.termomixLidl(3));
     }
+
     @Test
-    public void Test04() {
-        Assert.assertEquals("4", Main.termomixLidl(4));
-    }
-    @Test
-    public void Test05() {
+    public void Test_gacha() {
         Assert.assertEquals("Miga", Main.termomixLidl(5));
     }
+
     @Test
-    public void Test06() {
-        Assert.assertEquals("Gacha", Main.termomixLidl(6));
-    }
-    @Test
-    public void Test07() {
-        Assert.assertEquals("7", Main.termomixLidl(7));
-    }
-    @Test
-    public void Test08() {
-        Assert.assertEquals("8", Main.termomixLidl(8));
-    }
-    @Test
-    public void Test09() {
-        Assert.assertEquals("Gacha", Main.termomixLidl(9));
-    }
-    @Test
-    public void Test10() {
-        Assert.assertEquals("Miga", Main.termomixLidl(10));
-    }
-    @Test
-    public void Test11() {
-        Assert.assertEquals("11", Main.termomixLidl(11));
-    }
-    @Test
-    public void Test12() {
-        Assert.assertEquals("Gacha", Main.termomixLidl(12));
-    }
-    @Test
-    public void Test13() {
-        Assert.assertEquals("13", Main.termomixLidl(13));
-    }
-    @Test
-    public void Test14() {
-        Assert.assertEquals("14", Main.termomixLidl(14));
-    }
-    @Test
-    public void Test15() {
+    public void Test_gachamiga() {
         Assert.assertEquals("GachaMiga", Main.termomixLidl(15));
-    }
-    @Test
-    public void Test16() {
-        Assert.assertEquals("GachaMiga", Main.termomixLidl(30));
     }
 }
